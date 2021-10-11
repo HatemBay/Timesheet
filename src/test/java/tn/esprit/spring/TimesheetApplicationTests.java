b84d7c6766acbd703b96f5ad8b4a1080a220546b
Abdallah BEJI <Abdallah.beji@esprit.tn>
2021-10-11 17:13:58 +0100

    AjouteContrat

--- src/test/java/tn/esprit/spring/TimesheetApplicationTests.java
+++ src/test/java/tn/esprit/spring/TimesheetApplicationTests.java
@@ -1,13 +1,36 @@
 package tn.esprit.spring;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import tn.esprit.spring.entities.Contrat;
+//import tn.esprit.spring.services.IEmployeService
+import tn.esprit.spring.services.IEmployeService;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class TimesheetApplicationTests {
+public class TimesheetApplicationTests {
+	
+	@Autowired
+	IEmployeService empService ;
+	private static final Logger L = LogManager.getLogger(TimesheetApplicationTests.class);
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		
+		Contrat e = new Contrat(null, "CDI", 1500);
+		 empService.ajouterContrat(e);
+		 L.info("ajoute contrat");
 	}
 
 }
